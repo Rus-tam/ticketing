349d1515bc9101e52908e67e52ae6198da04afb3
Ахметов Рустам <ahmetov.rustam2011@gmail.com>
2022-11-21 19:05:22 +0500

    specifying the host

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,7 +1,31 @@
-const index = () => {
-    return (
-        <h1>Application</h1>
-    )
-}
+import axios from "axios";
+
+const index = ({ currentUser }) => {
+  console.log("Current User ", currentUser);
+  return <h1>Landing page</h1>;
+};
+
+index.getInitialProps = async () => {
+  if (typeof window === "undefined") {
+    // we are on the server!
+    // requests should be made to http://ingress-nginx.ingres
+    const { data } = await axios.get(
+      "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser",
+      {
+        headers: {
+          Host: "ticketing.dev",
+        },
+      }
+    );
+
+    return data;
+  } else {
+    // we are on the brouser
+    // requests can be made with a base url of ''
+    const { data } = await axios.get("/api/users/currentuser");
+
+    return data;
+  }
+};
 
 export default index;
